1d42a92443619ded2a0714f8d951affcd446ead7
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-01-16 17:36:14 -0800

    applying methods in apps

--- 01_Basics/app.js
+++ 01_Basics/app.js
@@ -5,6 +5,19 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org/'
         }; 
+    },
+    // Are callable to the program.
+    methods: {
+        //Must have parantheses
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'I know Vue!';
+            }
+
+        }
     }
 });
 
