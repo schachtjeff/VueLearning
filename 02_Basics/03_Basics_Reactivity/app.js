e1f2dfa3f377f247f8a1cd46830eec02188f366b
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-01-20 17:27:34 -0800

    adding the methods for data binding

--- 02_Basics/03_Basics_Reactivity/app.js
+++ 02_Basics/03_Basics_Reactivity/app.js
@@ -19,6 +19,12 @@ const app = Vue.createApp({
     resetInput() {
       this.name = '';
     },
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Schwarzmuller';
+    },
   }
 });
 
