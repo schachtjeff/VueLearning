0205568256ae2edfe7fbceb0dd360d052807833f
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-01-22 21:18:08 -0800

    watchers

--- 02_Basics/03_Basics_Reactivity/app.js
+++ 02_Basics/03_Basics_Reactivity/app.js
@@ -2,17 +2,42 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastname: '',
+      //fullname: ''
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    }
+    //The watch will execute whenever a data of same is run.
+    //The 'value' automatically comes for free in any watch.
+    /* name(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = value + ' ' + this.lastName;
+      }
+    },
+    lastName(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = this.name + ' ' + value;
+      }
+    } */
   },
   computed: {
     //it's a method property, but used like a data property.  Naming is same as data property.
     fullname() {
       console.log('Running again...');
-      if (this.name === '') {
+      if (this.name === '' || this.lastname === '') {
         return '';
       }
-      return this.name + ' ' + 'Schwarzmuller';
+      return this.name + ' ' + this.lastname;
     }
   },
   methods: {
