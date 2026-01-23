28778df385cd5f6be2b6e684047b29529d60b28d
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-01-22 20:47:20 -0800

    intro to computed properties

--- 02_Basics/03_Basics_Reactivity/app.js
+++ 02_Basics/03_Basics_Reactivity/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    //it's a method property, but used like a data property.  Naming is same as data property.
+    fullname() {
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Schwarzmuller';
+    }
+  },
   methods: {
     setName(event, lastName) {
       this.name = event.target.value;
